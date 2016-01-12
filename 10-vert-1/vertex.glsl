f64849daf7889cbb34cb33feabc2419fcc35ecc5
Scottland Donaldson <scott.p.donaldson@gmail.com>
2016-01-12 10:27:10 -0500

    rotating position in vertex shader

--- 10-vert-1/vertex.glsl
+++ 10-vert-1/vertex.glsl
@@ -6,7 +6,9 @@ attribute vec2 position;
 
 void main() {
 
-  //TODO: rotate position by theta radians about the origin
+  // had to look up rotation matrix here: https://en.wikipedia.org/wiki/Rotation_matrix
+  mat2 rotation = mat2(cos(theta), -sin(theta),
+  					   sin(theta),  cos(theta));
 
-  gl_Position = vec4(position, 0, 1.0);
+  gl_Position = vec4(position * rotation, 0, 1.0);
 }
