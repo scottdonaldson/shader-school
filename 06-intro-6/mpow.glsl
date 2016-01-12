70ef3da24388271c5444925228281205f210b20f
Scott Donaldson <scott.p.donaldson@gmail.com>
2016-01-11 19:21:41 -0500

    matrix multiplication

--- 06-intro-6/mpow.glsl
+++ 06-intro-6/mpow.glsl
@@ -1,13 +1,29 @@
 mat2 matrixPower(highp mat2 m, int n) {
   
-  //Raise the matrix m to nth power
+  mat2 m1 = m;
+  mat2 m2 = m1 * m1;
+  mat2 m4 = m2 * m2;
+  mat2 m8 = m4 * m4;
+  
+  mat2 result = mat2(1.0);
+  
+  if(n >= 8) {
+    n -= 8;
+    result *= m8;
+  }
+  if(n >= 4) {
+    n -= 4;
+    result *= m4;
+  }
+  if(n >= 2) {
+    n -= 2;
+    result *= m2;
+  }
+  if(n >= 1) {
+    result *= m1;
+  }
 
-  // For example:
-  //
-  //  matrixPower(m, 2) = m * m
-  //
-
-  return mat2(1.0);  
+  return result;
 }
 
 //Do not change this line or the name of the above function
