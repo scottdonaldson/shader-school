6e5e78081ed6935aac560fa33d05ac5892e58f92
Scottland Donaldson <scott.p.donaldson@gmail.com>
2016-01-12 12:16:52 -0500

    reflection matrix

--- 15-geom-4/reflect.glsl
+++ 15-geom-4/reflect.glsl
@@ -1,11 +1,9 @@
 highp mat4 reflection(highp vec3 n) {
-
-  //TODO: Return a matrix that reflects all points about the plane passing through the origin with normal n
-
-  return mat4(1, 0, 0, 0,
-              0, 1, 0, 0, 
-              0, 0, 1, 0,
-              0, 0, 0, 1);
+	n = normalize(n);
+	return mat4(1.0 - 2.0 * n.x * n.x, 		-2.0 * n.x * n.y,  	   -2.0 * n.x * n.z, 0,
+              		 -2.0 * n.x * n.y, 1.0 - 2.0 * n.y * n.y,  	   -2.0 * n.y * n.z, 0, 
+              		 -2.0 * n.x * n.z, 	    -2.0 * n.y * n.z, 1.0 - 2.0 * n.z * n.z, 0,
+              						0, 					   0, 					  0, 1);
 }
 
 #pragma glslify: export(reflection)
