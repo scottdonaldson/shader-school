e5d330f954475f024564693a9c9b80e161021ac0
Scottland Donaldson <scott.p.donaldson@gmail.com>
2016-01-12 10:23:03 -0500

    texture color manipulation

--- 09-frag-3/fragment.glsl
+++ 09-frag-3/fragment.glsl
@@ -4,9 +4,8 @@ uniform sampler2D texture;
 uniform vec2 screenSize;
 
 void main() {
-  vec2 coord = gl_FragCoord.xy / screenSize;
-
-  //TODO: Swap red and blue color channels of image
-
-  gl_FragColor = texture2D(texture, coord);
+  
+	vec2 coord = gl_FragCoord.xy / screenSize;
+	vec4 color = texture2D(texture, coord);
+	gl_FragColor = vec4(color.b, color.g, color.r, color.a);
 }
