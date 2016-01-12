8fa869509bb5d0261a94b136315eaa627c96069e
Scottland Donaldson <scott.p.donaldson@gmail.com>
2016-01-12 10:15:50 -0500

    checkerboard

--- 08-frag-2/fragment.glsl
+++ 08-frag-2/fragment.glsl
@@ -3,6 +3,14 @@ precision mediump float;
 void main() {
 
   //TODO: Replace this with a function that draws a checkerboard
-  
-  discard;
+
+	vec2 p = gl_FragCoord.xy;
+	p = step(0.5, fract(0.5 * p / 16.0));
+  	bool inTile = (p.x == p.y);
+
+  	if ( inTile ) {
+  		gl_FragColor = vec4(1);
+  	} else {
+  		discard;
+  	}
 }
